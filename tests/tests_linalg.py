517ff87aba54edad7bb82e5a3d787fe7ccc0c105
Diego Catalão <diego.catalao@gabriel.com.br>
2022-11-26 19:00:12 -0300

    chore: added github actions

--- tests/tests_linalg.py
+++ tests/tests_linalg.py
@@ -27,7 +27,6 @@ class LinalgTest(unittest.TestCase):
         self.assertEqual(linalg.dot([1], [1]), 1)
         self.assertEqual(linalg.dot([1, 1], [1, 1]), 2)
         self.assertEqual(linalg.dot([1, 2, 3, 4, 5], [6, 7, 8, 9, 0]), 80)
-        pass
 
     def test_argument_consistence_dot(self):
         with self.assertRaises(Exception):
